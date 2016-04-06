d4cc6e8a90eadd7c1ddf36a713f439c4723f1130
rohit <rohitgupta0810@gmail.com>
2016-04-06 15:52:06 -0600

    adding code to handle id

--- code/parseTeamStats.py
+++ code/parseTeamStats.py
@@ -9,13 +9,13 @@ abs_data_path1 = os.path.abspath(os.path.join(os.path.dirname(__file__), data_pa
 def process():
     output= ''
 
-    
+    c=0
     for f1 in sorted(os.listdir(data_path1)):
+        c = c+1
         file_path=os.path.abspath(os.path.join(abs_data_path1,f1))
         players = open(file_path)
         
         soup = BeautifulSoup(players,"lxml")
-        print soup.find('h1').text,
         
         stats =  soup.find('table',id,'team_stats_totals')
         rows = stats.find_all('tr')
@@ -23,21 +23,22 @@ def process():
         out = ''
         
         for row in rows:
-            
             cols = row.find_all('td')
             
             if len(cols) > 0:
                 if cols[1].text != 'NBA':
                     continue
                 
-                for col in cols:
+                for i,col in enumerate(cols):
                     out = out  +col.text+","
+                    if i == 2:
+                        print str(c)+','+col.text
                 
-                out = out + '\n'  
+                out = out + str(c) + '\n'  
         output = output + out
-        print out
+        #print out
                     
-    f = open('../data/teamsstat.csv','w')
+    f = open('../data/teamsstat_id.csv','w')
     f.write(output)
     
 process()        
